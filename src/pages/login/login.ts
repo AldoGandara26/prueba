0f859335a05eac2c3a3f3f348c0489f0723a761f
Aldo <pm557297@gmail.com>
2018-04-16 19:26:08 -0500

    gggg
    jjj

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { viewClassName } from '@angular/compiler';
 import {Provedor1Provider} from '../../providers/provedor1/provedor1';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -35,7 +36,24 @@ datos;
 )}
  
 signup(){
-  let f={user: this.datos.nombre,pass: this.datos.pass };
-  console.log(this.user);
+  let f={user: this.datos.nombre,password: this.datos.pass };
+  this.proveedor.obtenerDatos()
+  .subscribe(data=>{
+    if(user && pass== user && password){
+      alert("Bienvenido");
+      this.navCtrl.setRoot(HomePage)
+    }
+    else(){
+      alert("Usuario o contrasena no encontrados");
+    }
+
+
+  }, error =>{
+    console.log("ooops");
+  });
+
+  }
 }
-}
+
+
+
