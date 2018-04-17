88cb8ee61b4332201a370dea0a2cbefbafc49fc2
Luisss23 <luis.fernando.moreno.11@gmail.com>
2018-04-17 13:59:40 -0500

    tickets to create ticket

--- src/pages/tickets/tickets.ts
+++ src/pages/tickets/tickets.ts
@@ -1,12 +1,6 @@
+import { NuevoticketPage } from './../nuevoticket/nuevoticket';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TicketsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,9 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TicketsPage {
 
+  nuevoticketPage= "NuevoticketPage"
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad TicketsPage');
   }
