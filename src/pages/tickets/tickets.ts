8ac68afdd8dd12c9b2e3a90a3d9b43956144ce6d
Luisss23 <luis.fernando.moreno.11@gmail.com>
2018-04-17 15:23:24 -0500

    install dragula Modif Tickets

--- src/pages/tickets/tickets.ts
+++ src/pages/tickets/tickets.ts
@@ -1,7 +1,7 @@
 import { NuevoticketPage } from './../nuevoticket/nuevoticket';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 @IonicPage()
 @Component({
   selector: 'page-tickets',
@@ -11,10 +11,13 @@ export class TicketsPage {
 
   nuevoticketPage= "NuevoticketPage"
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private dragulaService: DragulaService) {
+    
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad TicketsPage');
   }
+
+ 
 
 }
