41b39b634ce93442ef1c8ad51c5f4261bc7eec5f
Luisss23 <luis.fernando.moreno.11@gmail.com>
2018-05-14 16:28:05 -0500

    nueva api

--- mysql-rest/src/models/user.js
+++ mysql-rest/src/models/user.js
@@ -8,43 +8,100 @@ connection=mysql.createConnection({
 });
 
 let userModel={};
-
-userModel.getUsers=(userData, callback)=>{
-	//console.log('1');
-	//console.log(userData);
-	if (connection) {
-		connection.query("SELECT * FROM users WHERE nombre_usuario = '"+userData.nombre+"';",
-		//connection.query("SELECT * FROM users ",
-			(err, rows) =>{
-
-				if (err) {
-					let error;
-					//console.log('usuario no encontrado');
-					//console.log('usuarios'.userData);
-
-					//usuario no encontrado
+//funcion para mostrar tickets
+userModel.a =function (callback)
+	{		
+		if (connection)
+			{
+				//SELECT * FROM tickets ORDER BY id  SELECT * FROM tickets WHERE estatus_ticket = '"+en revision+"'; 
+				connection.query("SELECT * FROM tickets WHERE estatus_ticket ='en revison'",
+				(err, rows) =>
+				{
+				if (err)
+				 {
+					throw err;
 				} else {
-
-					if (rows.pass_usuario == userData.pass_usuario) {
-					console.log("correctos");
-					//console.log(userData.pass_usuario);
+					console.log("estamos aqui");
+					return callback(null, rows);
+				}})	
+			}	
+	};
+	//funcion para el login
+userModel.h = function (userData,callback)
+{
+	if (connection)
+	{	
+		connection.query("SELECT * FROM users WHERE nombre_usuario = '"+userData.nombre+"';",
+			(err, rows) =>
+			{
+				if (err)
+				{
+					let error;
+					console.log('a ocurrido algo');
+				} 
+				else 
+				{
+					if(!rows)
+					{
+						console.log('no hay registros');
 					}
 					else
 					{
-						console.log("el usuario y la contra no coinciden");
-					//console.log(rows.pass_usuario);
+						if(rows[0] ==  undefined || rows[0] == "undefined")
+						{
+					
+							//let indefinido="indefinido";
+							return callback(null,"indefinido");
+						}
+						else
+						{
+							if ( rows[0].pass_usuario == userData.pass) 
+							{
+								return callback(null,'correcto');					
+							}
+							else
+							{ 
+								return callback(null,"nocoinciden");
+								console.log('usuario y contra no coinciden');
+							}
+						}
 					}
+<<<<<<< HEAD
+=======
 
 
 					//console.log(rows);
 
 
-					callback(null, rows);
+					(callback)(null, rows);
+>>>>>>> caf18e9342dd4924afd4e63057055b73f8f502a8
 				}
 			})
 	}
-
+	
 };
+<<<<<<< HEAD
+//funcion para insertar nuevos tickets
+userModel.b = function (insertData,callback)
+{
+	if (connection)
+	{
+			connection.query('INSERT INTO tickets SET ?', insertData,
+			(err,result)=>
+			{
+				if (err)
+				{
+					console.log(err);
+					//throw err;
+					return callback(null,"error");
+				} 
+				else
+				{
+					//console.log("estamos cerca");
+					return callback(null,"correcto");
+					//callback(null,
+					//{'insertId': result.insertId})
+=======
 
 userModel.insertUser = (userData, callback)=>{
 	console.log('ESTE E USER DATA')
@@ -56,12 +113,13 @@ userModel.insertUser = (userData, callback)=>{
 				if (err) {
 					throw err;
 				} else{
-					callback(null,
+					(callback)(null,
 					 {'insertId': result.insertId})
+>>>>>>> caf18e9342dd4924afd4e63057055b73f8f502a8
 				}
 			})
 	};
-}
+};
 
 
 module.exports = userModel;
--- mysql-rest/src/routes/userRoutes.js
+++ mysql-rest/src/routes/userRoutes.js
@@ -1,30 +1,69 @@
 const User = require('../models/user');
 
-module.exports=function (app){
-	app.get('/users', (req,res)=>{
-		User.getUsers((err, data)=>{
+module.exports=function (app)
+{
+	app.get('/users', (req,res)=>
+	{
+		User.a((err, data)=>
+		{
 			res.status(200).json(data);
 
-		}
-			);
-		
-
+		});
 	});
 
-	app.post('/users', (req, res) => {
-//console.log('jalando')
-//console.log('4');
-//console.log(req.body.nombre_usuario);
-	const userData={
-
+	app.post('/users', (req, res) => 
+	{
+		const userData=
+		{
 			id: null,
 			nombre: req.body.nombre_usuario,
-			pass: req.body.pass_usuario,
-			//textarea:req.body.textarea
-			//created_at:null,
-			//updated_at:null
-
+			pass:  req.body.pass_usuario			
 		}
+<<<<<<< HEAD
+		User.h(userData, function(err,us)
+		{
+		 console.log(us);
+
+		 if(us =="indefinido")
+		 {
+		 	res.send("indefinido");
+
+		 }
+		 else{
+		 	if (us =="nocoinciden"){
+		 	res.send(JSON.stringify({a: "nocoinciden" }));
+		 		console.log("no");
+
+		 	}
+		 	else{
+		 		if(us=="correcto"){
+		 			res.send(true);
+		 		}
+		 		else{
+		 			res.send("mal");
+		 		}
+		 	}
+		 }
+		 /*if ( us =="correcto")
+		 {
+		 	res.send(true);
+		 }
+		 else{		 	
+		 	if(us=="indefinido")
+		 	{		 		
+		 		res.send(false);
+		 	}
+		 	else
+		 	{
+		 		console.log('error');
+		 	}
+		 }*/
+
+
+
+		});
+
+=======
 		//console.log(userData);
 		 //console.log('estas aqui mor');
 		User.getUsers(userData, (err, data)=>{
@@ -33,7 +72,7 @@ module.exports=function (app){
 				//console.log(data, userData);
 				res.json({
 					success:true, 
-					msg: 'usuario Eencontrado',
+					msg: 'usuario Encontrado',
 					data: data
 				})
 			} else{
@@ -46,5 +85,31 @@ module.exports=function (app){
 
 
 		})
+>>>>>>> caf18e9342dd4924afd4e63057055b73f8f502a8
 	}); 
+
+	app.post('/insert', (req, res) => 
+	{
+		const insertData=
+		{
+			id: null,
+			nombre_ticket:req.body.nombre,
+			sistema:req.body.sis,
+			tipo:req.body.tipo,
+			descripcion_ticket:req.body.descripcion,
+			file:req.body.file,
+			comentario_id:req.body.comentario,
+			file2:req.body.file2,
+			created_at_ticket:null,
+			updated_at_ticket:null,
+			proyecto_id:null,
+			usuario_id:null,
+			estatus_ticket:"en revison"
+		}
+			console.log(insertData);
+			User.b(insertData, function(err,us)
+			{
+        		console.log("---->"+us);
+			});
+	});
 }
